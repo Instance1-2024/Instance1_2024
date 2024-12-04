9a895b562abc1fa5f31b1f1b18fa0cd979c44af7
Dunkan2205 <dunkan7274@gmail.com>
2024-12-04 12:14:01 +0100

    Adding Player change mesh
    I added the player change mesh but not with the layers yet

---
+++ Assets/Script/Runtime/ColorManangment/SCPlayerChangeColor.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using static Script.Runtime.SCEnum;
+namespace Script.Runtime.ColorManagment {
+    public class SCPlayerChangeColor : MonoBehaviour
+    {
+        private MeshFilter _meshFilter;
+        [SerializeField] private Mesh _meshBlack, _meshWhite, _meshGrey;
+        [SerializeField] private LayerMask _layerBlack, _layerWhite, _layerGrey;
+        void Start()
+        {
+            _meshFilter = transform.GetComponent<MeshFilter>();
+        }
+
+        public void ChangeColor(EColor color)
+        {
+            switch (color)
+            {
+                case EColor.White:
+                    ApplyColor(_meshWhite, _layerWhite);
+                    break;
+                case EColor.Black:
+                    ApplyColor(_meshBlack, _layerBlack);
+                    break;
+                case EColor.Grey:
+                    ApplyColor(_meshGrey, _layerGrey);
+                    break;
+            }
+
+        }
+        private void ApplyColor(Mesh mesh, LayerMask layer)
+        {
+            _meshFilter.mesh = mesh;
+        }
+    }
+}

328defcf1f3078223c2f76ff7bc9092ba005fbde
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-04 15:04:00 +0100

    Adding ColorChange Legacy

--- Assets/Script/Runtime/ColorManangement/SCChangeColor.cs
+++ Assets/Script/Runtime/ColorManangement/SCChangeColor.cs
@@ -1,28 +1,57 @@
-//using UnityEngine;
-//using static Script.Runtime.SCEnum
-//namespace Script.Runtime.Color {
+using UnityEngine;
+using UnityEngine.Serialization;
+using static Script.Runtime.SCEnum;
 
-//    public class ColorChange : MonoBehaviour
-//    {
-//        private MeshRenderer _meshRenderer;
-//        [SerializeField] private Mesh _meshBlack, _meshWhite, _meshGrey;
-//        void Start()
-//        {
-//            _meshRenderer = transform.GetChild(0).GetComponent<MeshRenderer>();
-//        }
+namespace Script.Runtime.ColorManagement {
 
-//        public void ChangeColor(EColor color);
-//        {
-//            switch(color){
-//            case EColor.White : 
-//            break;
-//            case Ecolor.Black : 
-//            break;
-//            case EColor.Grey : 
-//            break;
-//            }
-                
-//        }
+    public class SCColorChange : MonoBehaviour {
+        protected MeshFilter _meshFilter;
+        protected MeshRenderer _meshRenderer;
+        
+        [SerializeField] protected EColor _oldColor;
+        
+        [SerializeField] protected  SColor _black;
+        [SerializeField] protected SColor _white;
+        [SerializeField] protected SColor _gray;
+        
+        void Awake() {
+            _meshRenderer = transform.GetComponentInChildren<MeshRenderer>();
+            _meshFilter = transform.GetComponentInChildren<MeshFilter>();
+        }
 
-//    }
-//}
+        public void ApplyColor(SColor color) {
+            _meshFilter.mesh = color.Mesh;
+            _meshRenderer.material = color.Material;
+            int layerValue = (int)Mathf.Log(color.Layer.value, 2);
+            gameObject.layer = layerValue;
+        }
+        
+        public void ChangeColor(EColor color) {
+            if (_oldColor == color) {
+                Debug.Log( _oldColor);
+                return;
+            }
+            
+            switch (color) {
+                case EColor.White:
+                    ApplyColor(_white);
+                    break;
+                case EColor.Black:
+                    ApplyColor(_black);
+                    break;
+                case EColor.Gray:
+                    ApplyColor(_gray);
+                    break;
+            }
+
+            _oldColor = color;
+        }
+
+        [System.Serializable]
+        public struct SColor {
+            public Mesh Mesh;
+            public Material Material;
+            public LayerMask Layer;
+        }
+    }
+}
--- Assets/Script/Runtime/ColorManangement/SCPlayerChangeColor.cs
+++ Assets/Script/Runtime/ColorManangement/SCPlayerChangeColor.cs
@@ -1,76 +1,22 @@
 using Script.Runtime.InputSystem;
-using UnityEngine;
 using static Script.Runtime.SCEnum;
+
 namespace Script.Runtime.ColorManagement {
-    public class SCPlayerChangeColor : MonoBehaviour {
-        private MeshFilter _meshFilter;
-        private MeshRenderer _meshRenderer;
+    public class SCPlayerChangeColor : SCColorChange {
         private SCInputManager _inputManager => SCInputManager.Instance;
-
-        [SerializeField] EColor _oldColor;
-
-        [SerializeField] SColor _black;
-        [SerializeField] SColor _white;
-        [SerializeField] SColor _gray;
         
         void Start() {
-            _meshFilter = transform.GetComponentInChildren<MeshFilter>();
-            _meshRenderer = transform.GetComponentInChildren<MeshRenderer>();
-            
             ApplyColor(_white);
-            
             _inputManager.OnChangeColorEvent.Performed.AddListener(OnChangeColor);
         }
 
         void OnChangeColor() {
-            Debug.Log("S");
             if (_oldColor == EColor.Black) {
-                Debug.Log("Go to White");
                 ChangeColor(EColor.White);
             }
             else if (_oldColor == EColor.White) {
-                Debug.Log("Go to Black");
                 ChangeColor(EColor.Black);
             }
-        }
-        
-        public void ChangeColor(EColor color) {
-            if (_oldColor == color) {
-                Debug.Log( _oldColor);
-                return;
-            }
-                
-                
-            
-            switch (color) {
-                case EColor.White:
-                    Debug.Log("WHITE");
-                    ApplyColor(_white);
-                    break;
-                case EColor.Black:
-                    Debug.Log("BLACK");
-                    ApplyColor(_black);
-                    break;
-                case EColor.Gray:
-                    ApplyColor(_gray);
-                    break;
-            }
-
-            _oldColor = color;
-        }
-        
-        private void ApplyColor(SColor color) {
-            _meshFilter.mesh = color.Mesh;
-            _meshRenderer.material = color.Material;
-            int layerValue = (int)Mathf.Log(color.Layer.value, 2);
-            gameObject.layer = layerValue;
-        }
-        
-        [System.Serializable]
-        public struct SColor {
-            public Mesh Mesh;
-            public Material Material;
-            public LayerMask Layer;
         }
     }
 }

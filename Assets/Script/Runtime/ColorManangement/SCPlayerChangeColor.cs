b1223243ca0923124420880ad6d7c1688af5d969
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-04 17:04:21 +0100

    adding another ground check for the color

--- Assets/Script/Runtime/ColorManangement/SCPlayerChangeColor.cs
+++ Assets/Script/Runtime/ColorManangement/SCPlayerChangeColor.cs
@@ -1,13 +1,17 @@
 using Script.Runtime.InputSystem;
+using Script.Runtime.Player;
 using static Script.Runtime.SCEnum;
 
 namespace Script.Runtime.ColorManagement {
     public class SCPlayerChangeColor : SCColorChange {
         private SCInputManager _inputManager => SCInputManager.Instance;
+        private SCPlayerMovement _playerMovement;
         
         void Start() {
             _inputManager.OnChangeColorEvent.Performed.AddListener(OnChangeColor);
+            _playerMovement = GetComponent<SCPlayerMovement>();
             ChangeColor(_oldColor);
+            
         }
 
         void OnChangeColor() {
@@ -17,6 +21,33 @@ namespace Script.Runtime.ColorManagement {
             else if (_oldColor == EColor.White) {
                 ChangeColor(EColor.Black);
             }
+        }
+        
+        public override void ChangeColor(EColor color) {
+            switch (color) {
+                case EColor.White:
+                    ApplyColor(_white);
+                    ExcludeLayer(_black);
+                    ChangeGroundLayer(_white);
+                    
+                    break;
+                case EColor.Black:
+                    ApplyColor(_black);
+                    ExcludeLayer(_white);
+                    ChangeGroundLayer(_black);
+                    
+                    break;
+                case EColor.Gray:
+                    ApplyColor(_gray);
+                    
+                    break;
+            }
+
+            _oldColor = color;
+        }
+
+        void ChangeGroundLayer(SColor color) {
+            _playerMovement.ColorGroundLayer = color.Layer.value;
         }
     }
 }

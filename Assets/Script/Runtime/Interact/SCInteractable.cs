a1227b97e78d375662a881acb8cfc6b3e993dfda
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-09 10:05:11 +0100

    Fixing Pebble Interaction

--- Assets/Script/Runtime/Interact/SCInteractable.cs
+++ Assets/Script/Runtime/Interact/SCInteractable.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 
 namespace Script.Runtime.Interact {
     public class SCInteractable : MonoBehaviour, IInteractable {
-        [field:SerializeField]public Sprite Sprite { get; set; }
-        
+        [field:SerializeField] public Sprite Sprite { get; set; }
+        [field:SerializeField] public bool CanBeHold { get; set; }
+
         public void Interact() {
             Debug.Log(gameObject.name);
         }
---
+++ Assets/Script/Runtime/Pebble/SCPebble.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Script.Runtime.Interact;
+
+namespace Script.Runtime.Pebble {
+    public class SCPebble : SCInteractable {
+        private void Start() {
+            CanBeHold = true;
+        }
+    }
+}

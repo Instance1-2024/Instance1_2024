51389fad0d2b1a80223e05448eace0d3901f9bf3
Gabibel15022005 <153726789+Gabibel15022005@users.noreply.github.com>
2024-12-09 15:53:00 +0100

    Prefab Settings Video and Audio
    Add Settings Prefab (Video and Audio)
    Add AudioMixer
    Add AudioManager

---
+++ Assets/Resources/Prefabs/Options/Audio/AudioManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Audio;
+public class AudioManager : MonoBehaviour
+{
+    public AudioMixer TheMixer;
+    void Start()
+    {
+        if(PlayerPrefs.HasKey("MasterVol"))
+        {
+            TheMixer.SetFloat("MasterVol", PlayerPrefs.GetFloat("MasterVol"));
+        }
+
+        if(PlayerPrefs.HasKey("MusicVol"))
+        {
+            TheMixer.SetFloat("MusicVol", PlayerPrefs.GetFloat("MusicVol"));
+        }
+
+        if(PlayerPrefs.HasKey("SFXVol"))
+        {
+            TheMixer.SetFloat("SFXVol", PlayerPrefs.GetFloat("SFXVol"));
+        }
+        
+    }
+}

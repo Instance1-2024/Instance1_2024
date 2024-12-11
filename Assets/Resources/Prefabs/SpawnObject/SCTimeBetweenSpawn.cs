dddd250826a75b51d1f5353276c69c51bd9a6bfd
Gabibel15022005 <153726789+Gabibel15022005@users.noreply.github.com>
2024-12-11 13:08:03 +0100

    Auto Add of Script
    the spawner now add automatically the required script for lifespan to object it spawn

--- Assets/Resources/Prefabs/SpawnObject/SCTimeBetweenSpawn.cs
+++ Assets/Resources/Prefabs/SpawnObject/SCTimeBetweenSpawn.cs
@@ -14,6 +14,7 @@ public class TimeBetweenSpawn : MonoBehaviour
 
     void Update() {
         
+        //permet de choisir la manière de sapwn un objet
         if (!DespawnObjectOnDestroy )
         {
             Spawn1();
@@ -23,38 +24,57 @@ public class TimeBetweenSpawn : MonoBehaviour
             Spawn2();
         }
     }
-
+    /// <summary>
+    /// Ce spawn instantie un objet et le détruit toute les TimeBS
+    /// </summary>
     void Spawn1()
     {
+        //le timer qui commence a 0 par defauty ce qui fait spawn instantanément le premier objet
         _time -= Time.deltaTime ;
 
         if (_time <= 0) {
+            //reset le timer
             _time = TimeBS ;
 
+            //Instantie la prefab préciser dans l'éditeur
             GameObject prefab = Instantiate ( PrefabToSpawn ,  SpawnPos.position , Quaternion.identity );
+
+            //ajoute une force au spawn de l'objet
             prefab.GetComponent<Rigidbody>().AddForce(Direction.normalized * SpawnForce);
 
+            //ajoute le script de durée de vie au objet instantier
+            SCLifeSpanOfThisObject lifeComp = prefab.AddComponent<SCLifeSpanOfThisObject>();
             
-            if (prefab.TryGetComponent(out SCLifeSpanOfThisObject lifeComp))
-            {
-                lifeComp.LifeSpanObject = TimeBS;
-                lifeComp.SpawnerScript = GetComponent<TimeBetweenSpawn>() ;
-                lifeComp.canDespawn = true;
-            }
+            //donne le temp et le fait que le l'objet peut despawn
+            lifeComp.LifeSpanObject = TimeBS;
+            lifeComp.canDespawn = true;
+            
+            //ajoute une référence a ce script pour l'objet instantier
+            lifeComp.SpawnerScript = GetComponent<TimeBetweenSpawn>() ;
+
         }
     }
+    /// <summary>
+    /// Ce spawn instantie un objet quand CanSpawn == true ce qui est le cas au start et quand le précédent objet est détruit
+    /// </summary>
     void Spawn2()
     {
         if (CanSpawn == true)
         {
+            //empeche de faire spawn un objet tant que le précedent n'est pas détruit et donc renvoie dans OnDestroy : CanSpawn = true
             CanSpawn = false;
+
+            //Instantie la prefab préciser dans l'éditeur
             GameObject prefab = Instantiate ( PrefabToSpawn ,  SpawnPos.position , Quaternion.identity );
+
+            //ajoute une force au spawn de l'objet
             prefab.GetComponent<Rigidbody>().AddForce(Direction.normalized * SpawnForce);
+
+            //ajoute le script de durée de vie au objet instantier
+            SCLifeSpanOfThisObject lifeComp = prefab.AddComponent<SCLifeSpanOfThisObject>();
             
-            if (prefab.TryGetComponent(out SCLifeSpanOfThisObject lifeComp))
-            {
-                lifeComp.SpawnerScript = GetComponent<TimeBetweenSpawn>() ;
-            }
+            //ajoute une référence a ce script pour l'objet instantier
+            lifeComp.SpawnerScript = GetComponent<TimeBetweenSpawn>() ;
         }
 
     }

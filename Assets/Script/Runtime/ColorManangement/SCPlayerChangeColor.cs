692879c5b4d565e2a7b2b899da452f0b218aacea
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-11 14:07:03 +0100

    Collisions on platform kill the player

--- Assets/Script/Runtime/ColorManangement/SCPlayerChangeColor.cs
+++ Assets/Script/Runtime/ColorManangement/SCPlayerChangeColor.cs
@@ -1,3 +1,4 @@
+using System;
 using Script.Runtime.InputSystem;
 using Script.Runtime.Player;
 using UnityEngine;
@@ -8,13 +9,16 @@ namespace Script.Runtime.ColorManagement {
         private SCInputManager _inputManager => SCInputManager.Instance;
         private SCPlayerMovement _playerMovement;
         private SCPlayerHold _playerHold;
+        private CapsuleCollider _capsule;
+        [SerializeField] private float _capsuleOffset;
 
         [SerializeField] private LayerMask _throwingLayer;
-        
+
         void Start() {
             _inputManager.OnChangeColorEvent.Performed.AddListener(OnChangeColor);
             _playerMovement = GetComponent<SCPlayerMovement>();
             _playerHold = GetComponent<SCPlayerHold>();
+            _capsule = GetComponent<CapsuleCollider>();
             ChangeColor(_oldColor);
         }
 
@@ -31,7 +35,7 @@ namespace Script.Runtime.ColorManagement {
 
             ChangeItemColor();
         }
-        
+
         /// <summary>
         /// Changes the color and exclude the inverse layer from the collider and change the ground layer
         /// </summary>
@@ -42,41 +46,68 @@ namespace Script.Runtime.ColorManagement {
                     ApplyColor(_white);
                     ExcludeLayer(_black);
                     ChangeGroundLayer(_white);
-                    
+
                     break;
                 case EColor.Black:
                     ApplyColor(_black);
                     ExcludeLayer(_white);
                     ChangeGroundLayer(_black);
-                    
+
                     break;
                 case EColor.Gray:
                     ApplyColor(_gray);
-                    
+
                     break;
             }
 
             _oldColor = color;
+            DetectWall();
         }
 
         public override void ExcludeLayer(SColor color) {
             _collider.excludeLayers = color.Layer.value | _throwingLayer.value;
         }
-        
+
         /// <summary>
         /// Changes the ground layer
         /// </summary>
         /// <param name="color.Layer"> the layer to apply</param>
         void ChangeGroundLayer(SColor color) {
+
             _playerMovement.ColorGroundLayer = color.Layer.value;
         }
 
         void ChangeItemColor() {
-            if(!_playerHold.IsHolding)return;
-            if (_playerHold.HoldItem.TryGetComponent(out SCChangeColor _changeColor)){
+            if (!_playerHold.IsHolding) return;
+            if (_playerHold.HoldItem.TryGetComponent(out SCChangeColor _changeColor)) {
                 if (_changeColor.GetColor() == EColor.Gray) return;
                 _changeColor.ChangeColor(_oldColor);
             }
         }
+
+        void DetectWall() {
+            Vector3 capsuleCenter = _capsule.bounds.center;
+            float capsuleRadius = _capsule.radius * 0.9f; // Réduction de 10%
+            float capsuleHeight = _capsule.height * 0.9f; // Réduction de 10%
+        
+            // Calculer les points de la capsule
+            Vector3 point1 = capsuleCenter + Vector3.up * (capsuleHeight / 2 - capsuleRadius);
+            Vector3 point2 = capsuleCenter - Vector3.up * (capsuleHeight / 2 - capsuleRadius);
+        
+            // Direction et distance du cast
+            Vector3 direction = transform.forward;
+
+
+            RaycastHit[] hits = Physics.CapsuleCastAll(point1, point2, capsuleRadius, direction, 10f);
+            foreach (RaycastHit hit in hits) {
+                if (hit.transform.CompareTag("Platform")) {
+                    if (hit.transform.parent.GetComponent<SCChangeColor>().GetColor() == GetColor()) {
+                        GetComponent<SCPlayerRespawnAtCheckpoint>().OnRespawn();
+                    }
+                }
+            }
+        }
+
+
     }
 }

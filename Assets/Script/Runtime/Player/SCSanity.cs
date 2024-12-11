dad903a768e1ba997fa500b758af49cb91ddcbbd
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-11 17:22:40 +0100

    Adding Sanity

---
+++ Assets/Script/Runtime/Player/SCSanity.cs
@@ -0,0 +1,42 @@
+using Script.Runtime.ColorManagement;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityEngine.UI;
+
+namespace Script.Runtime.Player {
+    public class SCSanity : MonoBehaviour {
+        [SerializeField] Image _fillImage;
+        [SerializeField] float _maxSanity = 10f;
+        private SCChangeColor _changeColor;
+        [FormerlySerializedAs("_currentSanity")] public float CurrentSanity;
+        
+        private void Start() {
+            _changeColor = GetComponent<SCChangeColor>();
+            CurrentSanity = _maxSanity;
+            InvokeRepeating("AdjustSanity", 0.1f, 0.1f);
+        }
+        
+        private void AdjustSanity() {
+            if (ShouldReduceSanity()) {
+                UpdateSanity(CurrentSanity - 0.1f);
+            } else {
+                UpdateSanity(CurrentSanity + 0.1f);
+            }
+        }
+
+        private bool ShouldReduceSanity() {
+            return _changeColor.GetColor() == SCEnum.EColor.Black;
+        }
+        
+        public void UpdateSanity(float sanity) {
+            CurrentSanity = Mathf.Clamp(sanity, 0f, _maxSanity);
+            if (CurrentSanity == 0) {
+                GetComponent<SCPlayerRespawnAtCheckpoint>().OnRespawn();
+                CurrentSanity = _maxSanity;
+            }
+            _fillImage.fillAmount = CurrentSanity / _maxSanity;
+        }
+        
+    }
+}
+

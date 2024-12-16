4ac6c1ba8964d6ab2f766135d4d3ab14e144a02b
MDucazeaux <m.ducazeaux@lahorde.school>
2024-12-16 12:17:17 +0100

    Prophecy Animation setup finish

--- Assets/Script/Runtime/Prophecy/SCProphecyManager.cs
+++ Assets/Script/Runtime/Prophecy/SCProphecyManager.cs
@@ -11,8 +11,16 @@ namespace Script.Runtime.Prophecy
 
         public UnityEvent<int> GetMemoryEvent;
 
-        [SerializeField] private List<Transform> _propheciesTransform = new List<Transform>();
+        [SerializeField] private List<GameObject> _propheciesGO = new List<GameObject>();
+        private List<int> _memoryIDs;
 
+        [SerializeField] private float _timer = 5f;
+        private float _time = 0f;
+        
+        private bool _hasPlayed = false;
+
+        private int _id;
+        
         private void Awake()
         {
             if (Instance == null)
@@ -24,39 +32,54 @@ namespace Script.Runtime.Prophecy
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         private void Start()
         {
+            foreach (GameObject g in _propheciesGO)
+            {
+                g.gameObject.SetActive(false);
+            }
+            _memoryIDs = new List<int>();
+            
             GetMemoryEvent = new UnityEvent<int>();
             GetMemoryEvent.AddListener(Test);
         }
 
+        private void Update()
+        {
+            if (_id > 0 && _hasPlayed && !_propheciesGO[_id - 1].GetComponent<Animation>().IsPlaying(
+                    _propheciesGO[_id - 1].GetComponent<Animation>().clip.name))
+            {
+                _memoryIDs.Add(_id);
+
+                _time += Time.deltaTime;
+                if (_time >= _timer)
+                {
+                    foreach (int memoryID in _memoryIDs)
+                    {
+                        _propheciesGO[memoryID - 1].SetActive(false);
+                    }
+                    _hasPlayed = false;
+                    _time = 0f;
+                }
+            }
+        }
+
         private void Test(int i)
         {
-            // switch (i)
-            // {
-            //     case 1 :
-            //         _propheciesTransform[0].GetComponent<Animation>().Play();
-            //         break;
-            //     case 2 :
-            //         _propheciesTransform[0].GetComponent<Animation>().Play();
-            //         break;
-            //     case 3 :
-            //         _propheciesTransform[0].GetComponent<Animation>().Play();
-            //         break;
-            //     case 4 :
-            //         _propheciesTransform[0].GetComponent<Animation>().Play();
-            //         break;
-            //     case 5 :
-            //         _propheciesTransform[0].GetComponent<Animation>().Play();
-            //         break;
-            //     case 6 :
-            //         _propheciesTransform[0].GetComponent<Animation>().Play();
-            //         break;
-            //     case 7 :
-            //         _propheciesTransform[0].GetComponent<Animation>().Play();
-            //         break;
-            // }
-
-            _propheciesTransform[i - 1].GetComponent<Animation>().Play();
+            _id = i;
+            if (_memoryIDs.Count > 0)
+            {
+                foreach (int memoryID in _memoryIDs)
+                {
+                    _propheciesGO[memoryID - 1].gameObject.SetActive(true);
+                }
+            }
+            
+            _hasPlayed = true;
+            _propheciesGO[i - 1].SetActive(true);
+            _propheciesGO[i - 1].GetComponent<Animation>().Play(_propheciesGO[i - 1].GetComponent<Animation>().clip.name);
+            
         }
+        
+        
         
     }
 }

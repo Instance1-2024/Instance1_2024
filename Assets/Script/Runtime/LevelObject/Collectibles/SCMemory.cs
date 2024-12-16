f7fe556bfc294921e05ed672e398b4999b7cb4ec
MDucazeaux <m.ducazeaux@lahorde.school>
2024-12-16 12:23:28 +0100

    Update SCMemory.cs

--- Assets/Script/Runtime/LevelObject/Collectibles/SCMemory.cs
+++ Assets/Script/Runtime/LevelObject/Collectibles/SCMemory.cs
@@ -11,7 +11,8 @@ namespace Script.Runtime.LevelObject.Collectibles
 
         public bool CanBeHold { get; set; }
 
-
+        [Tooltip("Choose Between 1 and 7"), Range(1, 7), SerializeField]
+        private int _memoryID;
         /// <summary>
         /// Interaction between player and memory, destroy GO when Interact button is pressed
         /// </summary>

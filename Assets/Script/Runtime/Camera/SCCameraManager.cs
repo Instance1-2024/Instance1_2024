2af1e7fec9f3d48e09b4e0e10c29f10a9cf16d16
Gabibel15022005 <153726789+Gabibel15022005@users.noreply.github.com>
2024-12-16 14:25:48 +0100

    Change in CameraManager

--- Assets/Script/Runtime/Camera/SCCameraManager.cs
+++ Assets/Script/Runtime/Camera/SCCameraManager.cs
@@ -3,7 +3,6 @@ using UnityEngine.Events;
 public class SCCameraManager : MonoBehaviour
 {
     public static SCCameraManager Instance;
-    public float MinX, MaxX, MinY, MaxY ;
     [HideInInspector] public UnityEvent <float, float, float, float> ChangeCamera;
     void Awake() 
     {
@@ -12,18 +11,5 @@ public class SCCameraManager : MonoBehaviour
             Instance = this;
         }    
     }
-    void Start()
-    {
-        ChangeCamera.AddListener(ChangeCameraMinMax);
-    }
-    void ChangeCameraMinMax(float minX ,float maxX ,float minY ,float maxY )
-    {
-        Debug.Log(minX + maxX + minY + maxY);
-        
-        MinX = minX;
-        MaxX = maxX;
-        MinY = minY;
-        MaxY = maxY;
-
-    }
+    
 }
--- Assets/Script/Runtime/Camera/SCCameraMouvement.cs
+++ Assets/Script/Runtime/Camera/SCCameraMouvement.cs
@@ -6,16 +6,26 @@ public class SCCameraMouvement : MonoBehaviour
     [SerializeField] Transform _playerTransform;
     SCCameraManager _cameraManager;
     public float SmoothSpeed = 0.125f;
+    private float _minX, _maxX, _minY, _maxY ;
     void Start()
     {
         _transform = this.transform  ;
         _cameraManager = SCCameraManager.Instance ;
+        SCCameraManager.Instance.ChangeCamera.AddListener(ChangeCameraMinMax);
+    }
+    void ChangeCameraMinMax(float minX ,float maxX ,float minY ,float maxY )
+    {   
+        _minX = minX;
+        _maxX = maxX;
+        _minY = minY;
+        _maxY = maxY;
+
     }
     void Update()
     {
 
         // on garde le transform en Z car on ne veut pas que la camera soit au même niveau que le joueur
-        Vector3 desiredPosition = new Vector3( Mathf.Clamp(_playerTransform.position.x, _cameraManager.MinX, _cameraManager.MaxX), Mathf.Clamp(_playerTransform.position.y, _cameraManager.MinY, _cameraManager.MaxY), _transform.position.z);
+        Vector3 desiredPosition = new Vector3( Mathf.Clamp(_playerTransform.position.x, _minX, _maxX), Mathf.Clamp(_playerTransform.position.y, _minY, _maxY), _transform.position.z);
         
         // on créer le vecteur "Smooth" grace a Lerp et on ajoute * Time.deltatime pour toujours le faire a la même vitesse
         Vector3 smoothPosition = Vector3.Lerp(_transform.position , desiredPosition , SmoothSpeed * Time.deltaTime);

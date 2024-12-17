3e40b5e6b089a68f25191cda1576e2ab9243ea99
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-17 14:56:03 +0100

    Importing new Player Mesh and cage Mesh

---
+++ Assets/Script/Runtime/ColorManagement/SCMultipleMeshChangeColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using static Script.Runtime.SCEnum;
+
+namespace Script.Runtime.ColorManagement {
+    public class SCMultipleMeshChangeColor : MonoBehaviour, IChangeColor {
+        public EColor CurrentColor { get; set; }
+        public Collider Collider { get; set; }
+        public void ChangeColor(EColor color) {
+            throw new System.NotImplementedException();
+        }
+
+        public void ExcludeLayer(LayerMask layer) {
+            throw new System.NotImplementedException();
+        }
+    }
+}

f6c6c93eea36c386b16d3d3ed349091debb6d9bf
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-18 20:12:52 +0100

    Fix Death Paticle

--- Assets/Script/Runtime/Player/SCPlayerTooltip.cs
+++ Assets/Script/Runtime/Player/SCPlayerTooltip.cs
@@ -1,15 +1,20 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Script.Runtime.Player {
     public class SCPlayerTooltip : MonoBehaviour {
         [SerializeField] TextMeshProUGUI _tooltipText;
+        [SerializeField] Image _tooltipImage;
 
         private void Start() {
             _tooltipText.gameObject.SetActive(false);
+            _tooltipImage.gameObject.SetActive(false);
         }
 
         public TextMeshProUGUI GetTooltipText() => _tooltipText;
+        public Image GetTooltipImage() => _tooltipImage;
+        
     }
 }

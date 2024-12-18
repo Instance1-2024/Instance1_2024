d8243d084da6cdad7c522ae3ff2b036b7ff7a4cf
Lawice <129510417+Lawice@users.noreply.github.com>
2024-12-18 15:37:35 +0100

    Fix Levels

---
+++ Assets/Script/Runtime/ColorManagement/SCCageChangeColor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+using static Script.Runtime.SCEnum;
+using static UnityEngine.Random;
+
+namespace Script.Runtime.ColorManagement {
+    public class SCCageChangeColor : SCChangeColor {
+        [SerializeField] private MeshRenderer _monsterRenderer;
+
+        [SerializeField] private List<Material> _blackMonsters;
+        [SerializeField] private List<Material> _whiteMonsters;
+        
+        public override void ChangeColor(EColor color) {
+            switch (color) {
+                case EColor.White:
+                    ApplyColor(_white);
+                    ExcludeLayer(_black.Layer);
+                    ApplyMonsters(_whiteMonsters);
+                    break;
+                case EColor.Black:
+                    ApplyColor(_black);
+                    ExcludeLayer(_white.Layer);
+                    ApplyMonsters(_blackMonsters);
+                    break;
+                case EColor.Gray:
+                    ApplyColor(_gray);
+                    List<Material> _monsters = new(_blackMonsters);
+                    _monsters.AddRange(_whiteMonsters);
+                    ApplyMonsters(_monsters);
+                    break;
+            }
+
+            CurrentColor = color;
+        }
+
+        private void ApplyMonsters(List<Material> monsters) {
+            if (monsters.Count > 0) {
+                int randomIndex = Range(0, monsters.Count);
+                _monsterRenderer.material = monsters[randomIndex];
+            }
+        }
+    }
+}

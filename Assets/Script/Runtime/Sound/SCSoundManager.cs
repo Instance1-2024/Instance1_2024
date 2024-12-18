8c74dcb451c37ab61e790f625ced4d70ce7f8a1c
Amaya MARLY <92305133+A-Marly@users.noreply.github.com>
2024-12-18 11:33:49 +0100

    Prophetie Sounds

--- Assets/Script/Runtime/Sound/SCSoundManager.cs
+++ Assets/Script/Runtime/Sound/SCSoundManager.cs
@@ -2,32 +2,61 @@ using UnityEngine;
 
 public class SCSoundManager : MonoBehaviour
 {
+    [Header("Movements Related")]
     [SerializeField] private AudioSource _audioSourceWalk;
     [SerializeField] private AudioSource _audioSourceJump;
     [SerializeField] private AudioClip _clipLand;
     [SerializeField] private AudioClip _clipJump;
-    //[SerializeField] private AudioClip _walkClip;
-    //[SerializeField] private AudioClip _jumpUpClip; // when player leaves the floor
-    //[SerializeField] private AudioClip _jumpDownClip; // when the player hit the floor post jump
 
-    private void Awake()
+    [Header("Prophecy Related")]
+    [SerializeField] private AudioSource _audioSourceProphecy;
+    [SerializeField] private AudioClip _clipIncompleteProphecy;
+    [SerializeField] private AudioClip _clipCompleteProphecy;
+
+    public void AssembleProphecy()
     {
+        if (SCProphecyManager.Instance)
+            return;
+
+        if (SCProphecyManager.Instance.GetMemoryIds().Count < 7)
+            AssembleProphecyIncomplete();
+        else
+            AssembleProphecyComplete();
+    }
+
+    public void AssembleProphecyIncomplete()
+    {
+        if (!_audioSourceProphecy) return;
+
+        _audioSourceProphecy.clip = _clipIncompleteProphecy;
+        _audioSourceProphecy.Play();
+    }
+    public void AssembleProphecyComplete()
+    {
+        if (!_audioSourceProphecy) return;
+
+        _audioSourceProphecy.clip = _clipCompleteProphecy;
+        _audioSourceProphecy.Play();
     }
 
     public void PlayJumpUpSound()
     {
+        if (!_audioSourceJump) return;
+
         _audioSourceJump.clip = _clipJump;
         _audioSourceJump.Play();
     }
 
     public void PlayJumpDownSound()
     {
+        if (!_audioSourceJump) return;
         _audioSourceJump.clip = _clipLand;
         _audioSourceJump.Play();
     }
 
     public void PlayWalkSound()
     {
+        if (!_audioSourceWalk) return;
         _audioSourceWalk.Play();
     }
 }
